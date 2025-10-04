d660c546c4d6e16444f2c784ebb7fa219cc6afb0
VUMINHHAO256 <talavodich5555@gmail.com>
2025-10-04 16:23:49 +0700

    Add files via upload

---
+++ DeviceMaintenance.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeviceMaintenance.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
